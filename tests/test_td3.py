ff041b723037ad7098f83fe306f53a2a7be1f53f
jimkon <dkontzedakis@gmail.com>
2019-08-15 20:39:00 +0300

    experience replay

--- tests/test_td3.py
+++ tests/test_td3.py
@@ -26,11 +26,7 @@ class TestCritic(unittest.TestCase):
             self.assertTrue(False, "Update threw Exception {}".format(e))
 
         try:
-            self.critic.update(np.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]]),
-                               np.array([[0], [1], [2]]),
-                               np.array([0, 1, 2]),
-                               np.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]]),
-                               np.array([[0], [1], [2]]), update_targets=True)
+            self.critic.update_targets()
 
         except Exception as e:
             self.assertTrue(False, "Update threw Exception {}".format(e))
@@ -55,6 +51,37 @@ class TestActor(unittest.TestCase):
             self.actor.update(np.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]]))
         except Exception as e:
             self.assertTrue(False, "Update threw Exception {}".format(e))
+
+
+class TestExperienceReplay(unittest.TestCase):
+
+    def test_push(self):
+        f = np.arange
+        buffer = ExperienceReplay(10)
+        buffer.push(f(3), f(2), f(1), f(3))
+        s, a, r, s_ = buffer.get_random(1)
+        self.assertTrue(np.array_equal(s[0], f(3)))
+        self.assertTrue(np.array_equal(a[0], f(2)))
+        self.assertTrue(np.array_equal(r[0], f(1)))
+        self.assertTrue(np.array_equal(s_[0], f(3)))
+
+        self.assertEqual(buffer.length(), 1)
+        for i in range(2, 20):
+            buffer.push(f(3), f(2), f(1), f(3))
+            self.assertEqual(buffer.length(), min(i, 10))
+
+    def test_get_random(self):
+        f = np.arange
+        buffer = ExperienceReplay(10)
+        for i in range(2, 20):
+            buffer.push(f(3), f(2), f(1), f(3))
+
+        s, a, r, s_ = buffer.get_random(3)
+        self.assertTrue(len(s) == 3)
+        self.assertTrue(len(a) == 3)
+        self.assertTrue(len(r) == 3)
+        self.assertTrue(len(s_) == 3)
+
 
 
 if __name__ == '__main__':
